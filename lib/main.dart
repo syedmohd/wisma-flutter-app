046d340905998cbb0f30b9dd55429833802daa48
syedmohd <syedmohd@gmail.com>
2020-10-04 18:09:51 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text('My First App')),
-          body: Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/nasi_lemak.jpg'),
-                Text('Nasi Lemak')
+            appBar: AppBar(title: Text('EasyList')),
+            body: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10.0),
+                  child: RaisedButton(child: Text('Add Product')),
+                ),
+                Card(
+                    child: Column(
+                      children: <Widget>[
+                        Image.asset('assets/nasi_lemak.jpg'),
+                        Text('Nasi Lemak')
+                      ],
+                ))
               ],
-            ),
-          ),
-        ));
+            )));
   }
 }
